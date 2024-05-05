a131bdce99f60c99683ce0f48296ab2ce7502788
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-05-05 12:45:28 -0500

    Update Mini_Project.py

--- Mini_Project.py
+++ Mini_Project.py
@@ -8,7 +8,6 @@ def read_contacts():
         with open('contacts_list.txt', 'r') as file:
             for line in file:
                 data = re.search(r'([\w\s]+)-:-([\d-]+)-:-([\w\s@.]+)', line)
-                contacts[name] = {'Name': data.group(1), 'Phone Number': data.group(2), 'Email': data.group(3)}
     except FileNotFoundError:
         print('No Local files')
         return {}
@@ -16,43 +15,65 @@ def read_contacts():
         print('Importing Local Data...')
         return contacts
 
+
 def write_contacts(contacts):
     with open('contacts_list.txt', 'w') as file:
         for name, info in contacts.items():
-            file.write(f"{name}-:-{info['Phone Number']}-:-{info['Email']}\n")
+            file.write(f"{info['Name']}-:-{info['Phone Number']}-:-{info['Email']}\n")
+
 
 def add_contact(contacts):
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     name = input('Name of Contact: ')
-    number = input('number of contact: ')
-    email = input('Contacts Email: ')
-    contacts[name] = {'Phone Number': number, 'Email': email}
+    number = input('Number of Contact: ')
+    email = input('Contact Email: ')
+    contacts[name] = {'Name': name, 'Phone Number': number, 'Email': email}
     write_contacts(contacts)
     print(f'Added {name} to your contact list')
 
+
 def display(contacts):
-    os.system('cls')
+    os.system('cls' if os.name == 'nt' else 'clear')
     print('Contact List')
     print('------------')
-    for idx, info in enumerate(contacts.items()):
+    for idx, (name, info) in enumerate(contacts.items()):
         print(f"{idx+1}.) {info['Name']} Phone number is {info['Phone Number']} and their email is {info['Email']}")
+
+def edit_contact(contacts):
+    display(contacts)
+    option = int(input("Select which contact you want to edit: "))
+    contact = list(contacts.keys())[option-1]
+    print(f"Editing {contact}:")
+    new_name = input('New Name: ')
+    new_number = input('New Number: ')
+    new_email = input('New Email: ')
+    contacts[contact] = {'Name': new_name, 'Phone Number': new_number, 'Email': new_email}
+    write_contacts(contacts)
+    print(f"{contact} edited successfully!")
+
+def search_contact(contacts):
+    search_query = input("Enter the name of the contact: ").lower()
+    search_results = [info for name, info in contacts.items() if search_query in name.lower()]
+    if search_results:
+        print("Search Results:")
+        for idx, result in enumerate(search_results):
+            print(f"{idx+1}.) {result['Name']} Phone number is {result['Phone Number']} and their email is {result['Email']}")
+    else:
+        print("No contacts were found with that name.")
+
 
 def remove_contact(contacts):
     display(contacts)
-    option = input("Select which contact you want to remove: ")
-    contact = contacts.pop(option-1)
-    print(f"Removed {contact['Name']}!")
+    option = int(input("Select which contact you want to remove: "))
+    contact = list(contacts.keys())[option-1]
+    del contacts[contact]
+    print(f"Removed {contact}!")
     write_contacts(contacts)
 
 
-
-
-
-
 def management():
-    
-    contacts = {}
-    
+    contacts = read_contacts()
+
     while True:
         print('''
     Welcome to the Contact Management System! 
@@ -73,27 +94,26 @@ def management():
             if action == 1:
                 add_contact(contacts)
             elif action == 2:
-                pass
+                edit_contact(contacts)
             elif action == 3:
                 remove_contact(contacts)
             elif action == 4:
-                pass
+                search_contact(contacts)
             elif action == 5:
                 display(contacts)
-                pass
+            elif action == 6:
+                write_contacts(contacts)
+                print("Contacts exported successfully!")
+            elif action == 7:
+                contacts = read_contacts()
+                print("Contacts imported successfully!")
             elif action == 8:
                 print("Quitting Contact Application.")
                 break
             else:
                 print("Invalid Selection. Choose one of the given options.")
         except ValueError:
-            print("Numbers only BoZo")
-
-
-
-
-
-
+            print("Numbers only BoZo.")
 
 
 management()
